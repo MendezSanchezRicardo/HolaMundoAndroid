d39e82382d5bfa6ecc41a1eebf890f32de143509
Calculadora1_Actividad1 <mendez.sanchez.ricardo.antonio@gmail.com>
2021-11-05 09:26:48 -0600

    Tercera version (calculadora)

---
+++ app/src/main/java/com/ricardo/mendez/sanchez/logica/Calculadora.java
@@ -0,0 +1,21 @@
+package com.ricardo.mendez.sanchez.logica;
+
+import android.view.View;
+
+import java.io.Serializable;
+
+public class Calculadora implements Serializable {
+
+    public void suma(){
+
+    }
+    public void resta(){
+
+    }
+    public void multiplicacion(){
+
+    }
+    public void division(){
+
+    }
+}
